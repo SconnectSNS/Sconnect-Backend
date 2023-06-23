73d503618ca8e632e67f2318b0bf6eb6bdc1ec02
julymeltdown_ <quintuplets2000@gmail.com>
2023-06-23 23:43:22 +0900

    initial commit

---
+++ src/main/kotlin/com/sconnect/sconnect/SconnectApplication.kt
@@ -0,0 +1,11 @@
+package com.sconnect.sconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SconnectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SconnectApplication>(*args)
+}
